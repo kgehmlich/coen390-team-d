7d3eb03aa8e8418c4a19fd8069343e2b1023fb1c
Kyle Gehmlich <kgehmlich@gmail.com>
2017-03-06 13:42:55 -0500

    Create Android Studio Project
    Create project with empty activity

---
+++ HeartrateMonitor/app/src/main/java/com/coen390/team_d/heartratemonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coen390.team_d.heartratemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
