2b2a9840370b3b2f960725742949aecaf3246572
rustamante <tinconcordia@gmail.com>
2017-03-11 23:25:20 -0500

    ADD email notifications classes

--- app/src/main/java/com/coen390/team_d/heartratemonitor/MainActivity.java
+++ app/src/main/java/com/coen390/team_d/heartratemonitor/MainActivity.java
@@ -2,6 +2,9 @@ package com.coen390.team_d.heartratemonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.coen390.team_d.util.EmailNotification;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            EmailNotification sender = new EmailNotification("username@gmail.com", "password");
+            sender.sendMail("This is Subject",
+                    "This is Body",
+                    "user@gmail.com",
+                    "user@yahoo.com");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
     }
 }
