11ea5a13e71351058a24da4931aff18a66cd2dc9
rustamante <tinconcordia@gmail.com>
2017-03-12 20:15:09 -0500

    ADD email notifications classes

--- app/src/main/java/com/coen390/team_d/heartratemonitor/MainActivity.java
+++ app/src/main/java/com/coen390/team_d/heartratemonitor/MainActivity.java
@@ -3,6 +3,9 @@ package com.coen390.team_d.heartratemonitor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.coen390.team_d.util.EmailNotification;
 
@@ -12,14 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            EmailNotification sender = new EmailNotification("username@gmail.com", "password");
-            sender.sendMail("This is Subject",
-                    "This is Body",
-                    "user@gmail.com",
-                    "user@yahoo.com");
-        } catch (Exception e) {
-            Log.e("SendMail", e.getMessage(), e);
-        }
+
+        Button buttonNotification = (Button) findViewById(R.id.btnManual);
+        buttonNotification.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+qsa                toast.show();
+
+                // Email Notificacion call
+                try {
+                    EmailNotification sender = new EmailNotification("coen390teamd@gmail.com", "heartrate");
+                    sender.sendMail("message",
+                            "Test",
+                            "coen390teamd@gmail.com",
+                            "coen390teamd@gmail.com");
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+            }
+        });
+
+
+
     }
 }
