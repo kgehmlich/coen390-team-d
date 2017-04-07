0fec129667e0072505d39f9cb8d2d2720952a72d
Kyle Gehmlich <kgehmlich@gmail.com>
2017-04-07 11:55:40 -0400

    Refactoring and cleanup

--- app/src/main/java/com/coen390/team_d/heartratemonitor/TeamMonitoringActivity.java
+++ app/src/main/java/com/coen390/team_d/heartratemonitor/TeamMonitoringActivity.java
@@ -40,7 +40,7 @@ import java.util.concurrent.RunnableFuture;
 
 public class TeamMonitoringActivity extends AppCompatActivity {
 
-  private LineGraphSeries<DataPoint> series;
+    private LineGraphSeries<DataPoint> series;
 
 	private Handler updateHandler;
     private final static int UPDATE_DELAY_SECS = 10;
@@ -49,7 +49,7 @@ public class TeamMonitoringActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_team_monitoring);
-		
+
 		/*
 		//Create graph
 		GraphView graph = (GraphView) findViewById(R.id.graphTeam);
@@ -102,7 +102,7 @@ public class TeamMonitoringActivity extends AppCompatActivity {
     }
 
 
-  /**
+    /**
 	 * Adds toolbar menu to this activity
 	 */
 	@Override
@@ -171,21 +171,21 @@ public class TeamMonitoringActivity extends AppCompatActivity {
 		double x = 1;
 		double y = 1;
 		series.appendData(new DataPoint(x, y), true, 30);
-		
+
 	}*/
 
 
     // Class to asynchronously update the UI with data from server
-    private class FetchHeartRates extends AsyncTask<Void, Void, Integer> {
+    private class FetchHeartRates extends AsyncTask<Void, Void, Void> {
         private ArrayList<HeartRatesDO> hrList;
 
-        protected Integer doInBackground(Void... voids) {
+        protected Void doInBackground(Void... voids) {
             AWSDatabaseHelper dbHelper = new AWSDatabaseHelper(getApplicationContext());
             hrList = dbHelper.getListOfHeartRates();
-            return 0;
+            return null;
         }
 
-        protected void onPostExecute(Integer integer) {
+        protected void onPostExecute(Void v) {
             Toast.makeText(getApplicationContext(), "Heart rates updated (" + hrList.size() + ")", Toast.LENGTH_LONG).show();
             // populate list view with user heart rates
             ListView hrListView = (ListView) findViewById(R.id.listView);
